2618a1414828549ff40e7b400b16c71880717f3e
mikhailovnaaaaaaaaaaa <98839769+mikhailovnaaaaaaaaaaa@users.noreply.github.com>
2024-06-04 00:47:29 +0300

    Add files via upload

---
+++ Экзамен/Info/Заявка.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Экзамен.Info
+{
+    /// <summary>
+    /// Логика взаимодействия для Заявка.xaml
+    /// </summary>
+    public partial class Заявка : Page
+    {
+        public Заявка()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Меню());
+        }
+    }
+}
